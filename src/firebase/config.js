24feb7eab36a1e83fbfd5da9a26a0e408a5eb1cc
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2021-01-30 11:47:54 -0300

    - Added firebase authentication config;

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,13 +1,15 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
+import 'firebase/auth'
 
 const firebaseConfig = {
-    
+
   };
 
   firebase.initializeApp(firebaseConfig)
 
+  const projectAuth = firebase.auth()
   const projectFirestore = firebase.firestore()
   const timestamp = firebase.firestore.FieldValue.serverTimestamp
 
-  export { projectFirestore, timestamp}
+  export { projectAuth, projectFirestore, timestamp}
